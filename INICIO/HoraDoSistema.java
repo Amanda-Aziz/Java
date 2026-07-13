c6b3a7aff2a4471db43fc87b0df489519e6cfddb
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-07-13 11:25:16 -0300

    Update and rename OlaMundo.java to HoraDoSistema.java

---
+++ INICIO/HoraDoSistema.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * DESCRIÇÃO:
+ * Programa simples que captura a data e a hora atual do sistema operacional 
+ * e exibe as informações formatadas no terminal do usuário.
+ *
+ * FUNCIONAMENTO:
+ * 1. Importa a classe Date da biblioteca padrão do Java.
+ * 2. Instancia o objeto 'relogio' para coletar o horário atual da máquina.
+ * 3. Converte os dados para texto e imprime o resultado na tela.
+ *
+ * Autor: Amanda-Aziz
+ * Data: 13 de Julho de 2026
+ *******************************************************************************/
+package com.mycompany.horadosistema;
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();   /*Cria um objeto chamado 'relogio' baseado na ferramenta Date que foi importado no inicio.*/
+        System.out.println("A hora do sistema é: ");
+        System.out.println(relogio.toString());
+    }
+}
--- INICIO/OlaMundo.java
+++
@@ -1,7 +0,0 @@
-/* COMETARIO */
-package primeiroprograma;
-public class PrimeiroPrograma{
-    public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
-    }
-}
